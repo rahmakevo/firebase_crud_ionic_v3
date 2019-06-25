6e6634687dbc37a66b180acfb8b0629962bf7fa5
rahmakevo <siundu344@gmail.com>
2019-06-25 12:18:24 +0300

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { Observable } from 'rxjs/Observable';
+import { Note } from '../../model/note/note.model';
+import { NoteListService } from '../../services/note-list.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  noteList: Observable<Note[]>
+
+  constructor(public navCtrl: NavController, private noteListService: NoteListService) {
+    this.noteList = this.noteListService.getNoteList()
+      .snapshotChanges()
+      .map(
+      changes => {
+        return changes.map(c => ({
+          key: c.payload.key, ...c.payload.val()
+        }))
+      });
+  }
+}
